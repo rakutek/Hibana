9a87fd38c0fc577c32057092e2948dd1296438e6
rakutek <rakutek12@gmail.com>
2020-10-29 18:28:43 +0900

    Initial Commit

---
+++ Hibana/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hibana
+//
+//  Created by raku on 2020/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hibana/HibanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HibanaApp.swift
+//  Hibana
+//
+//  Created by raku on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct HibanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
