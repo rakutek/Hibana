9944a05531a50dbdad954da51ed43cdde947418a
rakutek <rakutek12@gmail.com>
2020-11-08 00:06:17 +0900

    時計動くように変更

--- Hibana/Views/ClockView.swift
+++ Hibana/Views/ClockView.swift
@@ -1,24 +1,17 @@
-//
-//  ClockView.swift
-//  Hibana
-//
-//  Created by raku on 2020/10/05.
-//
-
 import SwiftUI
 
 struct ClockView: View {
-        @State var hour = Calendar.current.component(.hour, from: Date())
-        @State var minute = Calendar.current.component(.minute, from: Date())
-        @State var second = Calendar.current.component(.second, from: Date())
-        
-        var body: some View{
-            
-            Text("\(hour):\(minute):\(second)")
-            .font(.title)
-            
-        }
+    @State var currentDate = Date()
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+    var body: some View {
+        Text("\(currentDate)")
+            .onReceive(timer) { input in
+                self.currentDate = input
+            }
     }
+    
+}
 
 struct ClockView_Previews: PreviewProvider {
     static var previews: some View {
