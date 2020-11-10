2c014c5b9818642e7cada60a54d100946c93739c
rakutek <rakutek12@gmail.com>
2020-11-11 00:18:30 +0900

    クラブ一覧設置

--- Hibana/ContentView.swift
+++ Hibana/ContentView.swift
@@ -4,18 +4,35 @@
 //
 //  Created by raku on 2020/10/29.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    let arr = ["VIZEL","CAMELOT","ATOM","EDGE HOUSE","TK","Beginning","DJMASAHIRO来日","ドラムン町内会"]
+    
     var body: some View {
-        VStack{
-            ClockView()
-            TimeTableView()
+        NavigationView{
+            
+            VStack {
+                
+                ForEach(0..<arr.count) { num in
+                
+                NavigationLink(destination: TimeTableView()) {
+                    Text(arr[num])
+                }
+                .foregroundColor(.primary)
+                .font(.title)
+                .padding()
+                
+                
+                }
+                
+            }
         }
-
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
