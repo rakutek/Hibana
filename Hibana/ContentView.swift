637f34664aae304269a56607bd2082cd672b07f9
rakutek <rakutek12@gmail.com>
2020-11-13 13:39:11 +0900

    タブ作成

--- Hibana/ContentView.swift
+++ Hibana/ContentView.swift
@@ -11,18 +11,51 @@ struct ContentView: View {
     let arr = ["VIZEL","CAMELOT","ATOM","EDGE HOUSE","TK","Beginning","DJMASAHIRO来日","ドラムン町内会"]
     
     var body: some View {
-        NavigationView{
-            VStack {
-                ForEach(0..<arr.count) { num in
-                    NavigationLink(destination: TimeTableView()) {
-                        Text(arr[num])
+        
+        
+
+        
+        
+        
+        TabView {
+            
+            
+            
+            
+                    NavigationView{
+                        VStack {
+                            ForEach(0..<arr.count) { num in
+                                NavigationLink(destination: TimeTableView()) {
+                                    Text(arr[num])
+                                }
+                                .foregroundColor(.primary)
+                                .font(.title)
+                                .padding()
+                            }
+                        }
                     }
-                    .foregroundColor(.primary)
-                    .font(.title)
-                    .padding()
-                }
+            
+            
+                .tabItem {
+                    Image(systemName: "1.square.fill")
+                    Text("イベント")
+            }
+            Text("Another Tab")
+                .tabItem {
+                    Image(systemName: "2.square.fill")
+                    Text("お気に入り")
+            }
+            Text("The Last Tab")
+                .tabItem {
+                    Image(systemName: "3.square.fill")
+                    Text("DJ検索")
             }
         }
+        .font(.headline)
+    
+        
+        
+        
     }
 }
 
